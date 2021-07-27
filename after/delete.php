f9f4527cd2ecf580c4ce4903124449c604f42d3f
HD-muhammad <66686580+HD-muhammad@users.noreply.github.com>
2021-07-27 12:01:04 +0700

    Add files via upload

---
+++ after/delete.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'functions.php';
+$pdo = pdo_connect();
+
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    header("location:index.php");
+    exit();
+} else {
+    die ('No ID specified!');
+}
+
+?>
